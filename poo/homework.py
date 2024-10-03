b0db01e1b1c0e6ea2b906348ee37e62de62ba196
ABEL JURADO <abel.jurado210399@gmail.com>
2024-10-03 10:04:35 -0500

    feet: ejercicios de poo
    realizamos ejercicios

---
+++ poo/homework.py
@@ -0,0 +1,52 @@
+# EJERCICIO 1
+# Crear una clase Banco
+# sus atributos seran nombre, apllidos, dni, numero de cuenta y saldo inicial
+# como metodos podemos hacer deposito retirar dinero y ver estado de cuenta
+
+class Banco:
+    def __init__(self,nombre,apellido,dni,numero_cuenta,saldo_inicial):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.dni=dni
+        self.numero_cuenta=numero_cuenta
+        self.saldo_inicial=saldo_inicial
+        # metodos
+    def deposita(self,monto):
+        print("esta depositando: S/. ", monto)
+    def retirar(self,monto):
+        print("esta retirando: S/. ",monto)
+    def ver(self):
+        print("ve su estado de cuenta")
+
+edith=Banco("edith","parinango",60414454,2324-2342-3334,100)
+edith.deposita(200)
+edith.retirar(90)
+edith.ver()
+
+# EJERCICIO 2
+# Crear una clase agencia
+# con sus atributos nombre y apellidos del pasajero dni numero de asiento fecha de viaje
+# sus metodos seran ingresar origen, ingresar destino, cancelar viaje, ver estado de pasaje
+
+class Agencia:
+    def init(self,nombre,apellido,dni,numero_asiento,fecha_viaje):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.dni=dni
+        self.numero_asiento=numero_asiento
+        self.fecha_viaje=fecha_viaje
+        # metodos
+    def origen(self,lugar):
+        print("ingresa origen: ", lugar)
+    def destino(self,lugar):
+        print("ingrese destino: ", lugar)
+    def cancelar_viaje(self):
+        print("viaje cancelado")
+    def estado_viaje(self):
+        print("ver su estado de viaje: ")
+#edith=Agencia("edith","parinango",60414454,5,2/10/24)
+edith=Agencia()
+edith.estado_viaje()
+edith.origen("puquio")
+edith.destino("arequipa")
+edith.cancelar_viaje()
